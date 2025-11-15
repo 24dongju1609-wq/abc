b453fde847c86a59950a6a331f19ff4d70c316ea
24dongju1609-wq <24dongju1609@dong-ju.ms.kr>
2025-11-15 11:30:46 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 import streamlit as st
 
 
-st.title('안녕하세')
+st.title('안녕하세요')
 
 
 st.write("여기에 오신 걸 환영합니다")
 
 
 if st.button('클릭'):
-    st.write("당신은 저에게 치킨을 사주셔야 합니")
+    st.write("당신은 저에게 치킨을 사주셔야 합니다")
 else:
     st.write("버튼을 클릭하세요.")
