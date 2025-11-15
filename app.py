35c1e6c3da463e21f8804b86d7a8cbee3891ad2b
24dongju1609-wq <24dongju1609@dong-ju.ms.kr>
2025-11-15 11:27:22 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import streamlit as st
 
-# 제목
+
 st.title('나의 첫 번째 Streamlit 앱')
 
-# 텍스트
+
 st.write("Streamlit을 사용하여 간단한 웹 앱을 만들었습니다!")
 
-# 버튼을 추가해서 클릭 시 텍스트 변화
+
 if st.button('클릭'):
     st.write("버튼이 클릭되었습니다!")
 else:
